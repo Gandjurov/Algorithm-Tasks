668635c44d78e7ae11a9ebfed7f281f808903b69
Gandjurov <gandjurov@abv.bg>
2019-05-10 19:39:56 +0300

    Add FindPalindromeOrNot

---
+++ FindPalindromeOrNot/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FindPalindromeOrNot
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstInput = "madam";
+            var secondInput = "step on no pets";
+            var thirdInput = "book";
+
+            CheckPalindrome(firstInput);
+            CheckPalindrome(secondInput);
+            CheckPalindrome(thirdInput);
+        }
+
+        private static void CheckPalindrome(string inputStr)
+        {
+            bool flag = false;
+
+            for (int i = 0, j = inputStr.Length - 1; i < inputStr.Length / 2; i++, j--)
+            {
+                if (inputStr[i] != inputStr[j])
+                {
+                    flag = false;
+                    break;
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+
+            if (flag)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+    }
+}
