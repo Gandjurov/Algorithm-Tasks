44b14fac6e478f3cc83db04483c15f1843956770
Gandjurov <gandjurov@abv.bg>
2019-05-10 21:45:33 +0300

    Add FindAllPossibleSubstring

---
+++ FindAllPossibleSubstring/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace FindAllPossibleSubstring
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstInput = "abcd";
+
+            FindAllSubstring(firstInput);
+        }
+
+        private static void FindAllSubstring(string inputStr)
+        {
+            for (int i = 0; i < inputStr.Length; i++)
+            {
+                StringBuilder subString = new StringBuilder();
+
+                for (int j = i; j < inputStr.Length; j++)
+                {
+                    subString.Append(inputStr[j]);
+                    Console.Write(subString + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
