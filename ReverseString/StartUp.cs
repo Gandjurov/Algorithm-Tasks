1cd71f12cf00f2e66d6de7607866b9ef435850a7
Gandjurov <gandjurov@abv.bg>
2019-05-11 13:29:48 +0300

    Add ReverseString

---
+++ ReverseString/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReverseString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = "hello world";
+            ReverseString(input);
+        }
+
+        private static void ReverseString(string inputStr)
+        {
+            char[] charArr = inputStr.ToCharArray();
+
+            for (int i = 0, j = inputStr.Length - 1;  i < j; i++, j--)
+            {
+                charArr[i] = inputStr[j];
+                charArr[j] = inputStr[i];
+            }
+
+            string reversedString = new string(charArr);
+
+            Console.WriteLine(reversedString);
+        }
+    }
+}
